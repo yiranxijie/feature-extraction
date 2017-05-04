03e25c0a0c0e89d2d99a91252788a9be5c65a5e4
tao <tao@gmail.com>
2017-05-04 22:18:06 +0800

    替换apkcrawler.py

--- py/apkcrawler.py
+++ py/apkcrawler.py
@@ -6,23 +6,22 @@ import urllib2
 import re
 
 
-localname="E:/apk/"
-txt_path="D:/tao/apkSample/downloadUrl(1).txt"
-error_path="E:/error.txt"
+localname="E:/apk2/"
+txt_path = "D:/tao/apkSample/testUrl.txt"
+error_path="E:/error2.txt"
 
 
 def download( output ):
 	#读取txt文件
 	f = open(txt_path,"r")  
-	#f2=open(error_path,"w")
-	lines = f.readlines() 
-	for i in range( 2 ):
+	f2=open(error_path,"w")
+	lines = f.readlines()
+	for i in range( len(lines) ):
 		if( i==0 ):
 			continue
 		else:
-			app_name=lines[12].split("\t")[0].decode('UTF-8').encode('GBK')
-			print app_name
-			if( app_name.find("/") ): #名字里面包含目录符号
+			app_name=lines[i].split("\t")[0]
+			if( app_name.find("/") ): #包含目录符号
 				p=re.compile(r'\/')
 				app_name=p.sub('_',app_name)
 			if( app_name.find("&") ): #名字里面包含&符号
@@ -31,25 +30,29 @@ def download( output ):
 			if( app_name.find(".") ): #名字里面包含.符号
 				p=re.compile(r'\.')
 				app_name=p.sub('_',app_name)
-			basicurl=lines[12].split("\t")[4]
+			basicurl=lines[i].split("\t")[4].strip('\n')
 			url=lines[i].split("\t")[4]
 			#print app_name+"\t"+basicurl
 			try:
 				print ("downloading..." + url + "  ** file is : "+ output)
 				response = urllib2.urlopen(url)
+				type=response.info().gettype()
 				newurl=response.geturl()
-				if( newurl.endswith("404.html") ):
-					#print "404"
-					f2.write(app_name+"\t"+basicurl)
-				else:
+				if( type.endswith(".package-archive")):		#正常的apk
 					apkpath=output+app_name+".apk"
-
 					resourceFile = open(apkpath, "wb")
 					resourceFile.write(response.read())
+					f2.write(app_name+"\t"+basicurl+"\t"+"normal:this is a normal apk"+"\n")
 					resourceFile.close()
-			except urllib2.HTTPError,e:
-				print e.code
-				print e.reason  
-				f2.write(app_name+"\t"+basicurl)
+				elif( newurl.endswith("404.html") ):	#最终地址转向一个404页面
+					#print "404"
+					f2.write(app_name+"\t"+basicurl+"\t"+"404_error:from newurl endswith 404.html"+"\n")
+				elif( type.endswith("text/html") ):		#无效的apk
+					f2.write(app_name+"\t"+basicurl+"\t"+"contentType_error:from content type text/html"+"\n")
+				else:
+					f2.write(app_name+"\t"+basicurl+"\t"+"unknow_error:unknow error"+"\n")
+			except:		#urlopen异常
+				# print e.code,";"+e.reason 
+				f2.write(app_name+"\t"+basicurl+"\t"+"exception_error:from urlopen exception"+"\n")
 			
 download( localname )
